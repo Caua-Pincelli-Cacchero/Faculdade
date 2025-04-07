0cf1ec2bf23169f3b9f05be1cef5bcb4628c43b6
Cauã Pincelli Cacchero <cauapincacchero@gmail.com>
2025-04-06 23:56:22 -0300

    exercicio19

---
+++ ExerciciosFaculdade/demo/src/main/java/faculdade/PG019A00.java
@@ -0,0 +1,17 @@
+package main.java.faculdade;
+import static javax.swing.JOptionPane.*;
+
+public class PG019A00 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            if (i % 2 == 0) {
+                showMessageDialog(null, "Os números pares são: " + i);
+            }
+        }
+        for (int i = 1; i <= 15; i++) {
+            if (i % 2 != 0) {
+                showMessageDialog(null, "Os números impares são: " + i);
+            }
+        }
+    }
+}
